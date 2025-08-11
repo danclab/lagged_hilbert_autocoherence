37f8e25bf93b466b53f8743e8264410361f7b053
jbonaiuto <jbonaiuto@gmail.com>
2025-08-11 20:27:28 +0200

    use age-specific alpha ranges determined from PSDs, only include subjects that are included in the main analysis (at least 5 trials in all conditions)

--- python/run_dev_umd_burst_detection.py
+++ python/run_dev_umd_burst_detection.py
@@ -1,4 +1,10 @@
-import sys
+import glob
+import os
+import os.path as op
+
+import scipy
+from fooof import FOOOF
+
 import utils
 import numpy as np
 import pandas as pd
@@ -9,7 +15,7 @@ from mne.io import read_epochs_eeglab
 from mne.preprocessing import peak_finder
 
 
-def process_epoch_alpha_bursts(epoch_path, list_of_sensors=[], name="infant-alpha"):
+def process_epoch_alpha_bursts(epoch_path, alpha_range, list_of_sensors=[], name="infant-alpha"):
     subject = epoch_path.parts[-3]
     age_label = processed_data_paths[0].parts[-6]
     if epoch_path.suffix == ".set":
@@ -26,7 +32,7 @@ def process_epoch_alpha_bursts(epoch_path, list_of_sensors=[], name="infant-alph
     trial_nos = np.arange(len(epochs))
     trial_selections = [utils.get_surrounding_elements(trial_nos, i, 9) for i in trial_nos]
 
-    epochs_filtered = epochs.filter(7, 13)
+    epochs_filtered = epochs.filter(alpha_range[0], alpha_range[1])
     hilbert_phase = epochs_filtered.copy().apply_hilbert(envelope=False)
     hilbert_envelope = epochs_filtered.copy().apply_hilbert(envelope=True)
     hilbert_phase = hilbert_phase.copy().get_data()
@@ -81,23 +87,169 @@ def process_epoch_alpha_bursts(epoch_path, list_of_sensors=[], name="infant-alph
     print(subject, "done!")
 
 
-if __name__ == '__main__':
+# Define helper to extract periodic component
+def extract_periodic(freqs, psd):
+    fm = FOOOF(peak_width_limits=[1, 8], max_n_peaks=6, verbose=False)
+    fm.fit(freqs, psd)
+    return fm.get_params('peak_params'), fm._peak_fit
 
-    try:
-        index = int(sys.argv[1])
-    except:
-        raise IndexError("no file index")
+
+def define_fois(freqs, mean_psd, debug=False):
+    mean_resid = mean_psd
+    foi_pk_freqs = []
+    foi_pk_vals = []
+    foi_fwhms = []
+    foi_ranges = []
+    orig_pks, properties = scipy.signal.find_peaks(mean_resid)
+    while True:
+        pks, properties = scipy.signal.find_peaks(mean_resid)
+
+        pk_vals = mean_resid[pks]
+        sorted_idx = np.argsort(-pk_vals)
+        pk_vals = pk_vals[sorted_idx]
+        pks = pks[sorted_idx]
+
+        pk_idx = pks[0]
+        pk_val = pk_vals[0]
+        pk_freq = freqs[pk_idx]
+
+        if pk_val < np.std(mean_resid):
+            if debug:
+                print('Peak val={}, thresh={}'.format(pk_val, .5 * np.std(mean_resid)))
+            break
+
+        l_idx = np.where(mean_resid[:pk_idx] <= pk_val * .5)[0]
+        r_idx = np.where(mean_resid[pk_idx:] <= pk_val * .5)[0]
+
+        if len(l_idx) and len(r_idx):
+            l_freq = freqs[l_idx[-1]]
+            r_freq = freqs[pk_idx + r_idx[0]]
+            r_side = (r_freq - pk_freq)
+            l_side = (pk_freq - l_freq)
+            fwhm = 2 * np.min([r_side, l_side])
+        elif len(l_idx):
+            l_freq = freqs[l_idx[-1]]
+            fwhm = 2 * (pk_freq - l_freq)
+        elif len(r_idx):
+            r_freq = freqs[pk_idx + r_idx[0] + 1]
+            fwhm = 2 * (r_freq - pk_freq)
+
+        l_freq = pk_freq - fwhm * .5
+        r_freq = pk_freq + fwhm * .5
+        sd = fwhm / (2 * np.sqrt(2 * np.log(2)))
+        A = pk_vals[0] * np.exp(-.5 * ((freqs - pk_freq) / sd) ** 2)
+        nearest_orig = np.min(np.abs(freqs[np.array(orig_pks)] - pk_freq))
+
+        if pk_val > 1.5 * np.std(mean_resid) and (
+                (pk_freq < 10 and fwhm > 1) or (pk_freq >= 10 and fwhm > 3)) and nearest_orig < 3:
+            print('Peak: freq={}, val={}, width={}, range={}-{}'.format(pk_freq, pk_val, fwhm, pk_freq - fwhm * .5,
+                                                                        pk_freq + fwhm * .5))
+            print('Distance to nearest original peak={}'.format(nearest_orig))
+
+            foi_pk_freqs.append(pk_freq)
+            foi_pk_vals.append(pk_val)
+            foi_fwhms.append(fwhm)
+            foi_ranges.append([l_freq, r_freq])
+        elif debug:
+            if not ((pk_freq < 10 and fwhm > 1) or (pk_freq >= 10 and fwhm > 3)):
+                print('Peak: freq={}, fhwm={} to narrow'.format(pk_freq, fwhm))
+            if pk_val <= 1.5 * np.std(mean_resid):
+                print('Peak: freq={} too low amplitude'.format(pk_freq))
+            if nearest_orig >= 3:
+                print('Peak: freq={} too far from original peaks'.format(pk_freq))
+
+        mean_resid = mean_resid - A
+        # mean_resid[mean_resid<0]=0
+    sorted_idx = np.argsort(foi_pk_freqs)
+
+    foi_pk_freqs = np.array(foi_pk_freqs)
+    foi_pk_vals = np.array(foi_pk_vals)
+    foi_fwhms = np.array(foi_fwhms)
+    foi_ranges = np.array(foi_ranges)
+
+    foi_pk_freqs = foi_pk_freqs[sorted_idx]
+    foi_pk_vals = foi_pk_vals[sorted_idx]
+    foi_fwhms = foi_fwhms[sorted_idx]
+    foi_ranges = foi_ranges[sorted_idx, :]
+
+    return foi_pk_freqs, foi_pk_vals, foi_fwhms, foi_ranges
+
+
+def determine_alpha_range(age):
+    epochs = {
+        'exe': ['EBM', 'LEXT', 'FTGE', 'EXGC', 'EXEND'],
+        'obs': ['OBM', 'LOBS', 'FTGO', 'OBGC', 'OBEND']
+    }
+    freq_lims = [1, 50]
+
+    out_path = op.join('/home/bonaiuto/lagged_hilbert_coherence/output/dev_beta_umd', age)
+
+    all_psd = []
+    sub_paths = glob.glob(op.join(out_path, 'sub-*'))
+    subjects_included=[]
+    for sub_path in sub_paths:
+        subj_id = os.path.split(sub_path)[-1]
+
+        epo_psd = []
+        has_all = True
+        for condition in ['exe', 'obs']:
+            for epo in epochs[condition]:
+                data_fname = op.join(sub_path, f'{subj_id}-{condition}-{epo}.npz')
+                if not op.exists(data_fname):
+                    has_all = False
+                    break
+                else:
+                    res = dict(np.load(data_fname))
+                    freqs = res['freqs']
+                    # Average PSD over channels
+                    mean_psd = np.mean(res['psd'], axis=0)
+                    # Average LC over trials then channels
+                    f_idx = (freqs >= freq_lims[0]) & (freqs <= freq_lims[1])
+                    freqs = freqs[f_idx]
+                    mean_psd = mean_psd[f_idx]
+                    epo_psd.append(mean_psd)
+            if not has_all:
+                break
+        if has_all:
+            print(f'{age}: {subj_id}')
+            all_psd.append(epo_psd)
+            subjects_included.append(subj_id)
+    all_psd = np.array(all_psd)
+
+    m_psd=np.mean(all_psd,axis=1)
+    periodic=[]
+    for s_idx in range(m_psd.shape[0]):
+        _, p = extract_periodic(freqs, m_psd[s_idx,:])
+        periodic.append(p)
+    periodic=np.array(periodic)
+    m_periodic=np.mean(periodic,axis=0)
+
+    foi_pk_freqs, foi_pk_vals, foi_fwhms, foi_ranges = define_fois(freqs, m_periodic)
+    foi_ranges = foi_ranges[np.argsort(foi_ranges[:, 0])]
+    alpha_range = None
+    for i in range(foi_ranges.shape[0]):
+        if foi_ranges[i, 0] > 3:
+            alpha_range = foi_ranges[i, :]
+            break
+    print(f'{age} alpha: {alpha_range}')
+    return subjects_included, alpha_range
+
+if __name__ == '__main__':
 
     set_log_level(verbose="ERROR")
 
-    dataset_dir = Path("/home/common/bonaiuto/dev_beta_umd/data/adult/derivatives/NEARICA_behav/")
-    output_dir = Path("/home/common/mszul/infant_alpha_bursts")
-    processed_data_paths = utils.get_files(
-        dataset_dir, "*.set",
-        strings=["tool", "obs", "exe", "eeg", "processed_data"]
-    )
+    output_dir = Path("../output/dev_beta_umd/alpha_bursts/")
 
-    epoch_path = processed_data_paths[index]
-
-    process_epoch_alpha_bursts(epoch_path)
+    ages=['9m','12m','adult']
+    for age in ages:
+        dataset_dir = Path("/home/common/bonaiuto/dev_beta_umd/data/"+age+"/derivatives/NEARICA_behav/")
+        processed_data_paths = utils.get_files(
+            dataset_dir, "*.set",
+            strings=["tool", "obs", "exe", "eeg", "processed_data"]
+        )
+        subjects_included, alpha_range=determine_alpha_range(age)
+        for epoch_path in processed_data_paths:
+            subject = epoch_path.parts[-3]
+            if subject in subjects_included:
+                process_epoch_alpha_bursts(epoch_path, alpha_range)
 
